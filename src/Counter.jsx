9ba36b3da9e78c8528853a1cb74a15e57da5058f
Giacomo Manzoni <99giaman99@gmail.com>
2021-05-13 19:19:01 +0200

    .

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -6,28 +6,43 @@ import { useEffect, useState } from 'react';
 import {tasks} from "./tasks"
 
 const AppStyled = styled.div`
-  display: grid;©
+  display: grid;
   grid-template-rows: auto 1fr;
   height: 100vh;
 `;
 
 function App({voices}) {
+  const [tasks, setTasks] = useState([])
+
   const [index, setIndex] = useState(0)
   useEffect(() => {
     setInterval(() => setIndex(index => ++index), 3000)
   }, [])
+  useEffect(() => {
+    fetch("https://40609ad7bcc9.ngrok.io/api/tasks").then(res => {return res.json()}).then(data => {setTasks(data)})
+  }, [])
 
-  if (index < tasks.vestire.length) {
-    return <AppStyled>
-        <Timeline/>
-        <Vestire actions={tasks.vestire} index={index} voices={voices}/>
-      </AppStyled>;
-  } else {
-    return <AppStyled>
-        <Timeline/>
-        <Mangiare actions={tasks.mangiare} index={index - tasks.vestire.length} voices={voices}/>
-      </AppStyled>;
+
+  if (tasks.length === 0){
+    return <h1>"vuoto"</h1>
   }
+  
+  return <AppStyled>
+       <Timeline/>
+        <Vestire actions={tasks[0].actions} index={index} voices={voices}/>
+      </AppStyled>;
+
+  // if (index < tasks.vestire.length) {
+  //   return <AppStyled>
+  //       <Timeline/>
+  //       <Vestire actions={tasks.vestire} index={index} voices={voices}/>
+  //     </AppStyled>;
+  // } else {
+  //   return <AppStyled>
+  //       <Timeline/>
+  //       <Mangiare actions={tasks.mangiare} index={index - tasks.vestire.length} voices={voices}/>
+  //     </AppStyled>;
+  // }
 }
 
 export default App
--- src/tasks.ts
+++ src/tasks.ts
@@ -1,16 +1,22 @@
 export const tasks = {
     "vestire": [
-        "Mutande",
+        /*"Mutande",
         "Pantaloni",
         "Maglietta",
         "Felpa",
-        "Calzini"
+        "Calzini"*/
     ],
 
     "mangiare": [
-        "Pizza",
+        /*"Pizza",
         "Pasta",
         "Broccoletti",
-        "Insalata"
+        "Insalata"*/
     ]
 }
+
+
+const tasks2 = [{ "actions": ["action1", "action2", "action3"], "_id": "609d4d5cd973f830040e1e05", "name": "nome1", "createdAt": "2021-05-13T16:01:32.028Z", "updatedAt": "2021-05-13T16:01:32.028Z", "__v": 0 },
+{ "actions": ["asdasd"], "_id": "609d544b59a646eb79cd0671", "name": "asdasd", "createdAt": "2021-05-13T16:31:07.867Z", "updatedAt": "2021-05-13T16:31:07.867Z", "__v": 0 },
+{ "actions": ["asdasd"], "_id": "609d545359a646eb79cd0672", "name": "asdasd", "createdAt": "2021-05-13T16:31:15.104Z", "updatedAt": "2021-05-13T16:31:15.104Z", "__v": 0 },
+{ "actions": ["action2"], "_id": "609d5bb1c3053ded64a043ad", "name": "nome2", "createdAt": "2021-05-13T17:02:41.559Z", "updatedAt": "2021-05-13T17:02:41.559Z", "__v": 0 }]
